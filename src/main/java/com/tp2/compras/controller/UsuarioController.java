dcf4ebe74a2ae03b70e7a1479ead1c90e0456832
davilb64 <davilopesbrito64@gmail.com>
2026-06-17 10:48:46 -0300

    Testes para Login - Código feito, testes passaram

--- src/main/java/com/tp2/compras/controller/UsuarioController.java
+++ src/main/java/com/tp2/compras/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.tp2.compras.controller;
 
 import com.tp2.compras.dto.UsuarioCadastroDTO;
+import com.tp2.compras.dto.UsuarioLoginDTO;
 import com.tp2.compras.model.Usuario;
 import com.tp2.compras.service.UsuarioService;
 import jakarta.validation.Valid;
@@ -38,6 +39,21 @@ public class UsuarioController {
         } catch (IllegalArgumentException e) {
             // Se o e-mail já existir, o serviço lança esta exceção, e nós retornamos HTTP 400 (Bad Request)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody UsuarioLoginDTO dto) {
+        try {
+            // Tenta autenticar
+            usuarioService.autenticar(dto);
+
+            // Se passar direto, retorna 200
+            return ResponseEntity.ok("Login realizado com sucesso!");
+
+        } catch (IllegalArgumentException e) {
+            // Se o Service lançar a exceção, devolve 401 Unauthorized
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
 }
--- src/main/java/com/tp2/compras/service/UsuarioService.java
+++ src/main/java/com/tp2/compras/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.tp2.compras.service;
 
 import com.tp2.compras.dto.UsuarioCadastroDTO;
+import com.tp2.compras.dto.UsuarioLoginDTO;
 import com.tp2.compras.model.Papel;
 import com.tp2.compras.model.Usuario;
 import com.tp2.compras.repository.UsuarioRepository;
@@ -50,5 +51,26 @@ public class UsuarioService {
 
         // Persistência
         return usuarioRepository.save(novoUsuario);
+    }
+
+    /**
+     * Realiza a autenticação do usuário para o Login.
+     * Retorna true se sucesso, ou lança exceção se as credenciais estiverem erradas.
+     */
+    public boolean autenticar(UsuarioLoginDTO dto) {
+        Assert.notNull(dto, "O objeto de login não pode ser nulo");
+
+        // Busca o usuário no banco. Se não achar, lança o erro genérico de segurança.
+        Usuario usuario = usuarioRepository.findByEmail(dto.email())
+                .orElseThrow(() -> new IllegalArgumentException("Credenciais inválidas."));
+
+        // Compara a senha digitada em texto puro com o Hash salvo no banco
+        boolean senhaCorreta = passwordEncoder.matches(dto.senha(), usuario.getSenhaHash());
+
+        if (!senhaCorreta) {
+            throw new IllegalArgumentException("Credenciais inválidas.");
+        }
+
+        return true;
     }
 }
