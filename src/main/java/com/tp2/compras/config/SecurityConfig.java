535c40481c43de9a1e81fb91bc1404c0568194b7
davilb64 <davilopesbrito64@gmail.com>
2026-06-17 09:29:30 -0300

    Adicionando Dockerfile para deploy no Render, além de pequenas alterações para integração

---
+++ src/main/java/com/tp2/compras/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.tp2.compras.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable) // Necessário para APIs REST
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/usuarios/cadastro").permitAll() // Libera o cadastro
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
